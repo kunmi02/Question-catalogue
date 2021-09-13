37eb49718787a196cb5d85ccb601d962cc8b0181
kunmi02 <mrkhunmie@gmail.com>
2021-09-13 07:53:23 +0100

    Removed redundant folders

--- srccc/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-// import recipesReducer from './recipe';
-import categoriesReducer from './categoryReducer';
-import filterReducer from './filterReducer';
-import mealsReducer from './mealReducer';
-
-export default combineReducers({
-  meals: mealsReducer, categories: categoriesReducer, filter: filterReducer,
-});
